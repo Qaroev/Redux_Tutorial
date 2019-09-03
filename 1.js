30e87d700241249cb25c4e5c4288879744c17c88
Bobojon <bqaroev@velox.biz>
2019-09-03 18:34:36 +0500

    commit

---
+++ 1.js
@@ -0,0 +1,22 @@
+let state = 0;
+
+
+function updateState(state, action) {
+    if (action === 'Increment') {
+        return state + 1
+    } else if (action === 'Decrement') {
+        return state - 1
+    } else {
+        return state
+    }
+}
+
+state = updateState(state, 'Increment');
+console.log(state);
+
+state = updateState(state, "Decrement");
+console.log(state);
+
+state = updateState(state, '');
+
+console.log(state)
